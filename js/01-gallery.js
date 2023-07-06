3d296cfce4c1119eca0cc32975dacec941f77baf
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-06 17:19:22 +0300

    gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,12 +18,26 @@ const markup = galleryItems.map(({preview, original, description}) =>
 galleryList.insertAdjacentHTML('beforeend', markup);
 
 galleryList.addEventListener('click', selectedItem);
+
 function selectedItem(event) {
     event.preventDefault();
+    if(event.target.nodeName !== "IMG") {
+        return;
+    }
+        const targetItem = event.target.dataset.source;
+    
+        const instance = basicLightbox.create(`
+        <img src="${targetItem}" width="800" height="600">
+    `)
+    instance.show() 
 
-    const targetItem = event.target.dataset.source;
+    galleryList.addEventListener("keydown", (event) => {
+        if(event.code === "Escape") {
+            instance.close()
+        }
+    })
+    }
+    
 
-    console.log(targetItem);  
-}
 
 console.log(galleryItems);
