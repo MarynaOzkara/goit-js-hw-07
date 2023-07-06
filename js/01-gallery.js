e1c77870670662ee7a8ffdce8dcad23f43dbdabd
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-06 16:27:43 +0300

    basicLightbow

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => 
+                 `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                  </li>
+                 `
+).join('');
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+galleryList.addEventListener('click', selectedItem);
+function selectedItem(event) {
+    event.preventDefault();
+
+    const targetItem = event.target.dataset.source;
+
+    console.log(targetItem);  
+}
+
 console.log(galleryItems);
